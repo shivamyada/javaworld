19caf737a1e61b98ee1189a44e5536ee29db92a5
shivam yadav <96403304+shivamyada@users.noreply.github.com>
2023-09-16 11:56:45 +0000

    date

---
+++ 01_basic/#7_date_and_time.js
@@ -0,0 +1,47 @@
+let mydate= new Date()
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
+
+
+console.log(mydate.toDateString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+
+
+let mydate2= new Date(2001,1,20)
+console.log(mydate2.toLocaleDateString());
+
+let time=Date.now()
+console.log(time);
+console.log(time.gettime);
+
