68427b0fd5ba36ce9b0bc58a298b7fd5a542e556
shivam yadav <96403304+shivamyada@users.noreply.github.com>
2023-09-18 07:45:01 +0000

    object

---
+++ OBJECT/#1object.js
@@ -0,0 +1,29 @@
+//const mysum= symbol("keys3")
+
+const shivam={ buletno:1234,
+              carno:" as3f5",
+             gmail:"sdfbgh2@gmail.com",
+                  coden:"gethub",
+                   crn:"dish",
+                 mysum:"keys"}
+console.log(shivam);
+console.log(shivam["carno"])
+
+// how can freez in object//
+  // object.freez("user")//
+
+   // function used in object
+
+   shivam.getting=function(){
+     console.log('hey dish ')
+   }
+    console.log(shivam.getting());
+    JsUser.greetingTwo = function(){
+        console.log(`Hello JS user, ${this.name}`);
+    }
+    
+    console.log(shivam.greeting());
+    console.log(shivam.greetingTwo());
+
+
+    
---
+++ OBJECT/#2object.js
@@ -0,0 +1,56 @@
+// const tinderUser = new Object()
+const instaUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "dfgh"
+tinderUser.isLoggedIn = false
+
+// console.log(instaUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shivam",
+            lastname: "yadav"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(instaUser);
+
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));
+
