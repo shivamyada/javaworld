1f45f4a3f31054d73ac2bc0f0c48d1c565040a90
shivam yadav <96403304+shivamyada@users.noreply.github.com>
2023-09-18 13:33:49 +0000

    function

---
+++ FUNCTION/01_function.js
@@ -0,0 +1,60 @@
+                        // snytax of function
+
+
+       
+  // subtract two number
+     function subtractnum(num1,num2){
+        console.log(num1 - num2);
+        return
+   }
+      subtractnum(2,4)
+
+      // add two number
+
+    function subtractnum(num1,num2){
+        console.log(num1 + num2);
+     }
+      const sub=subtractnum(3,8)
+      console.log(sub);
+
+  // we have two use
+     function subtractnum(num1,num2){
+         let sub=num1 - num2
+         return sub
+     }
+      const sub=subtractnum(3,8)
+      console.log(sub);
+
+      //only use return
+
+    function subtractnum(num1,num2){
+         return num1 + num2
+     }
+     const ror=subtractnum(3,8)
+      console.log(ror);
+  //  new types 
+
+
+    function usernm(whatyrname){
+        return `${whatyrname}what is ur name`
+    }
+     
+     console.log(usernm("shivam"));
+
+     function name ( parameter12, parameters2, parameters2)  {
+        // code to9 be executed                  
+       //                 }
+
+    //    function cooktea(tea, water,sugar,){
+    //     console.log("your tea will ne3 ready"+ tea+ "ingraduentuse"+ water + "cup ooooooof water"+  sugar)
+    //    }
+    //        cooktea( 4,2, 5);
+
+    //        // 2nd  program 
+
+    //        function sayshivam(){
+    //         console.log("shivani")
+    //         console.log("shivani2")
+    //         console.log("shivani3")
+    //        }
+    //        sayshivam();
